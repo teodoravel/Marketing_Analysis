b5960b76ad04e891544155da8401b852992c6b4c
teodoravel <veljanoskateodora9@gmail.com>
2024-12-14 00:31:53 -0500

    Initial commit with RStudio project files

---
+++ packages.R
@@ -0,0 +1,8 @@
+install.packages("rmarkdown")
+install.packages("knitr")
+install.packages("dplyr")
+install.packages("ggplot2")
+install.packages("reader")
+tinytex::install_tinytex()
+install.packages("readxl")
+
